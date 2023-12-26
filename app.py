e48048118d7a9a780d57f1a2c6457682b81ae32e
Maria Alekseeva <alek.maria@yandex.ru>
2023-12-27 02:25:02 +0300

    новая логика и поисковый движок

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ import streamlit as st
 from model.model import MyModel
 
 @st.cache_resource()
-def load_model(df_path: str = 'path/to/df') -> MyModel:
+def load_model(df_path: str = 'data/with_clusters.csv', n: int = 5) -> MyModel:
     """Логика загрузки модели машинного обучения.
 
     Args:
         df_path (str, optional): Путь до датафрейма в котором будет производится поиск. Defaults to 'path/to/df'.
+        n (int, optional): Количество соседей в методе юлижайших соседей для алгоритма кластеризации. Defaults to 5. 
 
     Returns:
         MyModel: Загруженная модель.
     """
-    return MyModel(model_path, df_path)
+    return MyModel(df_path, n)
 
 def load_df(df_path: str = 'data/worked_data.csv') -> pd.DataFrame:
     """Логика загрузки датафрейма в страницу.
@@ -25,20 +26,50 @@ def load_df(df_path: str = 'data/worked_data.csv') -> pd.DataFrame:
         pd.DataFrame: Предобработанный датафрейм.
     """
     df = pd.read_csv(df_path)
-    df = df.drop(columns = ['Unnamed: 0', 'product_id'])
+    df = df.drop(columns = ['Unnamed: 0'])
+    df = df[df['name'].notna()]
     return df
 
+def prod_id_to_dict(df: pd.DataFrame, id: str) -> dict:
+    """Преобразование id продукта к словарю с его свойствами из датафрейма.
+
+    Args:
+        df (pd.DataFrame): Датафрейм из которого будет искаться продукт.
+        id (str): ID продукта (должен присутствовать в датафрейме).
+
+    Returns:
+        dict: _description_
+    """
+    if id in df['product_id'].values:
+        result = df[df['product_id'] == id].drop(columns = ['product_id'])
+        result = result.to_dict(orient='records')[0]
+    else:
+        result = {}
+    return result
+
+def find_product_id(name: str, kitchen: str, geohash: str) -> str:
+    """_summary_
+
+    Args:
+        name (str): _description_
+        kitchen (str): _description_
+        geohash (str): _description_
+
+    Returns:
+        str: _description_
+    """
 
 def main():
     # Sidebar Search
     form = st.sidebar.form(key='Search')
     form.header("Поисковый запрос")
 
-    search_text = form.text_input('Введите название товара:')
+    geohash_search = form.selectbox('Выберите геохэш еды:', st.session_state.geohash)
+    kitchen_search = form.selectbox('Выберите кухню:', st.session_state.kitchen)
+    name_search = form.selectbox('Выберите название блюда:', st.session_state.product)
     form.form_submit_button('Поиск')
 
     # Sidebar Contributors
-
     with st.sidebar.expander('Команда разработки'):
         st.image('data/team.jpg')
         st.markdown('''
@@ -48,23 +79,27 @@ def main():
                     ''')
 
     # Main Logic App 
-    st.title("Приложениее для поиска товаров")
+    st.title("Приложениее для поиска товаров-заменителей")
 
     if 'df' not in st.session_state:
         st.session_state.df = load_df()
+        st.session_state.geohash = sorted(st.session_state.df['geohash'].unique().tolist())
+        st.session_state.kitchen = sorted(st.session_state.df['primary_cuisine'].unique().tolist())
+        st.session_state.product = sorted(st.session_state.df['en_name'].unique().tolist())
 
-    # Логика работы с ml моделью
-    model = load_model()
-    search_df = model.search(product_id)
     
     # Логика отображения
+    # Продукт, найденный по критериям поиска
     st.text('Найденный продукт:')
-    # Заглушка для примера
-    pred = st.session_state.df.sample().to_dict(orient='records')[0]
+    pred = prod_id_to_dict(st.session_state.df, product_id)
     st.write(pred)
     
+    # Поиск товаров заменителей
+    model = load_model()
+    search_list = model.search(product_id) 
+    search_df = st.session_state.df[st.session_state.df['product_id'].isin(search_list)].drop(columns = ['product_id'])
+    
     st.text('Топ 5 самых похожих продуктов:')
-    # Заглушка для примера
     st.dataframe(search_df, hide_index=True)
 
 if __name__ == '__main__':
--- model/model.py
+++ model/model.py
@@ -2,21 +2,23 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 class MyModel:
-    def __init__(self, df_path: str) -> None:
-        # Здесь может быть долгая загрузка модели model_path
-        self.df = pd.read_csv('../data/with_clusters.csv')
+    """Класс модели,осуществлающий поиск товара заменителя.
+    """
+    def __init__(self, df_path: str = '../data/with_clusters.csv', n: int = 5) -> None:
+        self.df = pd.read_csv(df_path)
+        self.n = n
         
-        
+
     def search(self, product_id: str) -> list:
-        product = df[df['product_id'] == product_id]
+        product = self.df[self.df['product_id'] == product_id]
         cluster_id = product['cluster'].values[0]
         product = product.drop(columns = ['product_id', 'cluster'])
-        cluster = df[df['cluster'] == cluster_id]
+        cluster = self.df[self.df['cluster'] == cluster_id]
         cluster = cluster.reset_index(drop=True)
-        knn = NearestNeighbors(n_neighbors=n)
+        knn = NearestNeighbors(n_neighbors=self.n)
         knn.fit(cluster.drop(columns = ['product_id', 'cluster']))
         neighbors = knn.kneighbors(product, 5, return_distance=False)
         answer = []
         for i in neighbors[0]:
-            answer.append(cluster.loc[i])
-        return pd.DataFrame(columns=cluster.columns.to_list(), data=answer)
+            answer.append(cluster.loc[i]['product_id'])
+        return answer
